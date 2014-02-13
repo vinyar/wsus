0f7a697c51477d1cd1864e55d1a71700d6116d8c
Alex Vinyar <alexv@opscode.com>
2014-02-12 17:39:27 -0800

    starting work on the client

--- recipes/configure_wsus.rb
+++ recipes/configure_wsus.rb
@@ -1,16 +1,3 @@
 ### Start setting up powershell commands to scan/configure server
 
 
-## MOdify GPO value of the new node to point at the new WSUS server
-
-# do some magical search to identify the WSUS server for the Chef Org
-# Populate the appropriate powershell string below
-
-powershell_script "point at the WSUS server" do
-	code <<-EOH
-    Get-GPPrefRegistryValue -Guid <Guid> -Context {<User> | <Computer>} -Key <string> [-Domain <string>] [-Order <int>] [-Server <string>] [-ValueName <string>] [<CommonParameters>]
-    Get-GPRegistryValue -Guid <Guid> -Key <string> [-Domain <string>] [-Server <string>] [-ValueName <string>] [<CommonParameters>]
-  EOH
-
-
-# Get the registry location for WSUS server from GPO cookbook in DOD STIG
